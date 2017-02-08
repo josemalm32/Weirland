10a86b472aeaa120cf4936ccd432a88f6adfd36a
Mario <Mario@Maroto>
2017-02-08 08:50:00 +0100

    Maroto Un cambio

--- src/Artesano.java
+++ src/Artesano.java
@@ -3,6 +3,11 @@ public class Artesano extends Secundario{
 
 	String direccionTaller;
 	String especialidad;
+	
+	
+	
+	
+	
 	public Artesano(String nombre, int numeroWeird, int diaNacimiento, int ciudadNacimiento, String sexo,
 			int numeroLicencia, int anioComienzoActividad, String nombreExplotacion, boolean licenciaCasamiento,
 			int numeroRelacion, String direccionTaller, String especialidad) {
